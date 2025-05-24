e5ba9c5b834b7e72ab564fcdf75288063132e59e
venraij <me@nickvanraaij.com>
2025-05-24 20:21:11 +0200

    more styling for transactions

--- src/app/tabs/transactions/transactions.page.ts
+++ src/app/tabs/transactions/transactions.page.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, IonList } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, IonList, IonIcon } from '@ionic/angular/standalone';
 import { Observable } from 'rxjs';
 import { ITransaction } from 'src/app/interfaces/transaction.interface';
 import { Store } from '@ngrx/store';
+import { addIcons } from 'ionicons';
+import { addOutline, removeOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-transactions',
   templateUrl: 'transactions.page.html',
   styleUrls: ['transactions.page.scss'],
-  imports: [IonList, IonItem, IonLabel, IonHeader, IonToolbar, IonTitle, IonContent, CommonModule]
+  imports: [IonIcon, IonList, IonItem, IonLabel, IonHeader, IonToolbar, IonTitle, IonContent, CommonModule]
 })
 export class TransactionsPage {
 
@@ -18,6 +20,7 @@ export class TransactionsPage {
   constructor(
     private store: Store<{ transactions: ITransaction[] }>
   ) {
+    addIcons({ addOutline, removeOutline });
     this.$transactions = this.store.select('transactions');
   }
 
