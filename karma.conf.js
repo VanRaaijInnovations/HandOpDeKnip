6c90d4e68d18563775f2d0f12fb2c953640f5239
venraij <me@nickvanraaij.com>
2025-11-16 17:28:20 +0100

    Fix for karma

--- karma.conf.js
+++ karma.conf.js
@@ -29,7 +29,8 @@ module.exports = function (config) {
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' }
+        { type: 'text-summary' },
+        { type: 'lcovonly' }
       ]
     },
     reporters: ['progress', 'kjhtml'],
@@ -37,8 +38,8 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
+    browsers: process.env.CI ? ['ChromeHeadless'] : ['Chrome'],
+    singleRun: process.env.CI ? true : false,
     restartOnFileChange: true
   });
 };
